f0138c90e59cec521782125d64e5c69850cf9fa8
Piotr F <fiodor.piotr@gmail.com>
2022-03-02 16:02:10 +0100

    first, after remake

---
+++ main.py
@@ -0,0 +1,245 @@
+import time
+
+import numpy
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTreeWidgetItem, QInputDialog
+from mainWindow_ui import Ui_MainWindow
+from purpul_be import *
+from utils import *
+
+#ROOT = os.path.dirname(sys.executable)
+ROOT = os.path.dirname(__file__)
+
+
+class mainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.ui = Ui_MainWindow() #klasa dziediczy po Ui_MainWindow, aiwec nie trtzeba tak definiowac
+        self.setupUi(self)
+        self.connect_gui_elements()
+
+        print(f'Drajwery w pyodbc to:{pyodbc.drivers()}')
+        self.db = None
+        self.dest_path = None
+        self.raportyGui = []
+        self.adr_for_lines = {
+            'RDLP': (self.rdlp, 2),
+            # find Qline edit, integer to dlugosc jaka dana kategoria powinna miec
+            'NADL': (self.ndl, 2),
+            'OBR': (self.obr, 1),
+            'LCTWO': (self.lctw, 2),
+            'ODDZ': (self.oddz, 6),
+            'PODODDZ': (self.pododdz, 4),
+            'WYDZ': (self.wydz, 2)
+        }
+        self.create_raports(self.raportTree, RAPORTS)
+
+    def connect_gui_elements(self):
+        self.dbButton.clicked.connect(self.get_data_base)
+        self.raportButton.clicked.connect(self.get_raport_dest)
+        self.run.clicked.connect(self.final)
+
+    def check_chosen_raports(self):
+        count = 0
+        wybrane_raporty = []
+        for raport_category in RAPORTS:
+            for raport_name in RAPORTS[raport_category]:
+                sheet_name = RAPORTS[raport_category][raport_name][2]
+                raport_gui_element = RAPORTS[raport_category][raport_name][3]
+                RAPORTS[raport_category][raport_name][1] = False  # reset statusów
+                if raport_gui_element.checkState(0) == Qt.Checked:
+                    RAPORTS[raport_category][raport_name][1] = True
+                    count += 1
+                    wybrane_raporty.append(sheet_name)
+        return count, wybrane_raporty
+
+    def create_raports(self, raportTree, raportDict):
+        for key in raportDict.keys():
+            parent = QTreeWidgetItem(raportTree)
+            parent.setText(0, f'{key}')
+            parent.setFlags(parent.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
+            parent.setFlags(parent.flags() & ~Qt.ItemIsSelectable)
+            self.raportyGui.append(parent)
+            parent.setDisabled(True)
+            for value in raportDict[key]:
+                child = QTreeWidgetItem(parent)
+                child.setFlags(child.flags() | Qt.ItemIsUserCheckable)
+                child.setFlags(child.flags() & ~Qt.ItemIsSelectable)
+                child.setText(0, f'{value}')
+                child.setCheckState(0, Qt.Unchecked)
+                if key == 'Raporty':
+                    raportDict[key][value].append(child)
+
+    def get_data_base(self):
+        self.base_path, self.base_ext = QFileDialog.getOpenFileName(caption="Wskaż Bazę", filter="Baza (*.mdb)")
+        if self.base_path:
+            self.baseLineEdit.setText(self.base_path)
+            self.db = connect_db(self.base_path)[0]
+            wydz_Data = get_table_data(self.db, SQL['WYDZ'])
+            self.adres = wydz_Data['TEMP_ADRESS_FOREST'].values[0]
+            adr_for_dict = create_adr_for_dict(self.adres, list=False)
+            for raport_Cat in self.raportyGui:
+                raport_Cat.setDisabled(False)
+            for raport_category in RAPORTS:
+                for raport_name in RAPORTS[raport_category]:
+                    raport_gui_element = RAPORTS[raport_category][raport_name][3]
+                    raport_gui_element.setDisabled(False)
+
+            for keya in self.adr_for_lines:
+                for keyb in adr_for_dict:
+                    if keya == keyb:
+                        self.adr_for_lines[keya][0].setText(adr_for_dict[keyb])
+            message('Sukces', f'Połączono z bazą danych {os.path.basename(self.base_path)}',
+                    QMessageBox.Information)
+
+    def get_raport_dest(self):
+        default_dir = os.path.join(ROOT, 'results')
+        path = QFileDialog.getExistingDirectory(caption='Wskaż scieżkę do utworzenia raportu',
+                                                directory=default_dir)
+        if path:
+            self.dest_path = path
+            self.raportLineEdit.setText(self.dest_path)
+
+    def construct_adr_for(self):  # dobrze robiacy adr_FOR bardzo fajne rozwiazanie
+        adr_for = ''
+        for key in self.adr_for_lines:
+            qline_content, correct_lenght = self.adr_for_lines[key]
+            if len(qline_content.text()) != correct_lenght and len(qline_content.text()) != 0:
+                return message('Błąd w długości pola w adr_for',
+                               f'Pole {key} ma złą długość, sprawdź i popraw. Uważaj na spacje',
+                               QMessageBox.Warning), False
+            else:
+                if qline_content.text().isspace() or len(qline_content.text()) == 0:
+                    adr_for += '%'
+                    break
+                else:
+                    if key != 'WYDZ':
+                        adr_for += (qline_content.text() + '-')
+                    else:
+                        adr_for += qline_content.text()
+        adr_for_dict = create_adr_for_dict(adr_for, False)  # create dict by set adr_For
+        for keya in self.adr_for_lines:  # ustaw nowe wartosci sprecyzowane przez uzytkownika ( tylko wyglad, nie ma
+            # wplywu na ostatefczny adr_For ktory zostanie przekazany do kwerendy
+            for keyb in adr_for_dict:
+                if keya == keyb:
+                    self.adr_for_lines[keya][0].setText(adr_for_dict[keyb])
+                    if self.adr_for_lines[keya][0].text() == '%':
+                        self.adr_for_lines[keya][0].setText('')
+
+        return adr_for, True
+
+    def raport_name_dialog(self):
+        text, result = QInputDialog.getText(self, 'Raport', 'Podaj nazwę raportu:')
+        if result:
+            return text
+
+    def final(self):
+        if not self.db:
+            message('Nie widzę bazy', 'Wskaż bazę danych i spróbuj ponownie', QMessageBox.Warning)
+        elif not self.dest_path:
+            message('Nie widzę ścieżki docelowej', 'Wskaż ścieżkę do utworzenia raportu i spróbuj ponownie',
+                    QMessageBox.Warning)
+        else:
+            raport_name_user = self.raport_name_dialog()
+            adr_for = self.construct_adr_for()
+            if raport_name_user and adr_for[1] and not raport_name_user.isspace():
+
+                # adr_for = self.construct_adr_for()
+                count, wybrane_raporty = self.check_chosen_raports()  # ilosc wybranych raportow
+                print(wybrane_raporty)
+                new_raport = copy_blank_raport(ROOT, self.dest_path, raport_name_user)
+                delete_empty_raports(new_raport, wybrane_raporty)
+                add_logo(new_raport, 'logo.png', 'A1')
+                add_logo(new_raport, 'footer.png', 'C46')
+                # zwraac adres i status
+                filter = adr_for[0]
+
+                if adr_for[1]:  # must be true
+                    dialog, bar = progdialog(0, 'Wysyłam zapytanie SQL',
+                                             f'Rysuję tabele...\n Muszę narysować: {count} tabel')
+                    bar.setValue(0)
+                    bar.setMaximum(100)
+                    QApplication.processEvents()
+                    i = 0  # wartosc i pomocnicza do ustawiania progress bara
+                    for raport_category in RAPORTS:
+                        for raport_name in RAPORTS[raport_category]:
+                            sql_ = RAPORTS[raport_category][raport_name][0]
+                            sheet_name = RAPORTS[raport_category][raport_name][2]
+                            if RAPORTS[raport_category][raport_name][1]:  # #teoretycznie moznabyloby sprawdzic check
+                                # tak jak w w
+                                # check_chosen_raports, ale niech będzie, chyba tak jest troszkę szybciej. CHYBA
+                                # print(raport_gui_element, raport_name, sql_)
+
+                                if raport_name in ['Uprawy otwarte', "Uprawy i młodniki po rębniach złoż."]:
+                                    sql1 = """SELECT F_ARODES.ARODES_INT_NUM, F_ARODES.TEMP_ADRESS_FOREST, F_SUBAREA.STAND_STRUCT_CD, F_AROD_STAND_PEC.FOREST_PEC_CD, F_SUBAREA.SUB_AREA, [skrocony_opis-taks].ot INTO UPR_MLO_ZLOZ FROM (F_ARODES INNER JOIN (F_SUBAREA INNER JOIN [skrocony_opis-taks] ON F_SUBAREA.ARODES_INT_NUM = [skrocony_opis-taks].ARODES_INT_NUM) ON F_ARODES.ARODES_INT_NUM = F_SUBAREA.ARODES_INT_NUM) INNER JOIN F_AROD_STAND_PEC ON F_SUBAREA.ARODES_INT_NUM = F_AROD_STAND_PEC.ARODES_INT_NUM WHERE (((F_ARODES.TEMP_ADRESS_FOREST) Like ?) AND ((F_AROD_STAND_PEC.FOREST_PEC_CD) In ('upr złoż','mło złoż')) AND ((F_ARODES.TEMP_ACT_ADRESS)=True)) ORDER BY F_ARODES.ORDER_KEY; """
+                                    cursor = self.db.cursor()
+                                    if cursor.tables(table='UPR_MLO_ZLOZ', tableType='TABLE').fetchone():
+                                        cursor.execute('DROP TABLE UPR_MLO_ZLOZ')
+                                        self.db.commit()
+                                    cursor.execute(sql1, filter)
+                                    self.db.commit()
+                                    cursor.close()
+                                if raport_name in ['Rozbieżności']:
+                                    data = get_table_data(self.db, sql_, [filter, filter])
+                                    data = data.sort_values(by=['PARCEL_NR','UZ','TEMP_ADRESS_FOREST'])
+                                    data.insert(0, 'Lp', range(0, 0 + len(data)))
+                                    print(data.columns)
+
+                                else:
+                                    data = get_table_data(self.db, sql_, [filter])
+
+                                if 'Adres leśny' in data.columns:  # skracanie adresu
+                                    data['Adres leśny'] = data['Adres leśny'].apply(shorten_adr_for)
+                                if 'TEMP_ADRESS_FOREST' in data.columns:  # skracanie adresu
+                                    data['TEMP_ADRESS_FOREST'] = data['TEMP_ADRESS_FOREST'].apply(shorten_adr_for)
+                                if 'NATURE_MON_FL' in data.columns:  # zmiana osobliwosci przyrodniczych na stringi
+                                    data = data.astype({"NATURE_MON_FL": str})
+                                    data = data.replace({'NATURE_MON_FL': {'False': 'Nie', 'True': 'Tak'}})
+
+                                data.loc['Suma'] = data.sum(numeric_only=True)  # zsumowanie floatow
+                                last_row = pd.IndexSlice[data.index[data.index == "Suma"], : ]  # ostatni row (suma
+                                # do ustawienia stylu)
+                                #
+                                # for col in data.columns:
+                                #     if col in ['Pow','SPECIAL_AREA', 'SumaOfSUB', 'Pow wskazania', 'SUB_AREA','pow','PROT_SITE_AREA',]:
+                                #         print('jestem w round 2')
+                                #         data[col] = data[col].round(2)
+                                #     if col in ['AROD_LAND_USE_AREA','LAND_USE_AREA']:
+                                #         print('jestem w round 4')
+                                #         data['AROD_LAND_USE_AREA'] = data['AROD_LAND_USE_AREA'].round(4)
+
+
+
+
+                                data = data.replace(r'\r\n',' ')
+                                data = data.style. \
+                                    applymap(set_bold, subset=last_row). \
+                                    applymap(set_green, subset=last_row). \
+                                    applymap(set_white_text, subset=last_row). \
+                                    applymap(set_font_size)# ustawianie styli na dataframe w pandas - te cechy excel odziediczy
+
+                                to_excel(data, new_raport, sheet_name)
+                                headers = get_column_names(sheet_name,excel=new_raport)
+                                set_style(new_raport, sheet_name)
+                                set_format(new_raport, sheet_name,headers)
+                                # ustaw zmiany na progress barze
+                                i += 1
+                                progress = i / count * 100
+                                bar.setValue(int(progress))
+                                QApplication.processEvents()
+                                if progress == 100:
+                                    time.sleep(0.5)
+                                    pytanie = msg_question(f'Sukces {raport_name_user}',
+                                                           'Ukończono tworzenie raportu, zamknąć aplikacje?',
+                                                           self.centralwidget)
+                                    if pytanie:
+
+                                        sys.exit()
+        print('ZROBIONO')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = mainWindow()
+    window.show()
+    app.setStyle('fusion')
+    app.exec_()
---
+++ purpul_be.py
@@ -0,0 +1,240 @@
+import copy
+import os
+import shutil
+import sys
+import openpyxl
+import pandas as pd
+import pyodbc
+from openpyxl.styles import Border, Side, Font, Alignment
+from styleframe import StyleFrame, Styler, utils
+from dictionaries import *
+
+
+ROOT = os.path.dirname(sys.executable)
+
+
+def connect_db(db_path):
+    odbc_drivers = [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
+    print(odbc_drivers)
+    try:
+        driver = u"{Microsoft Access Driver (*.mdb, *.accdb)}"
+        print('poszedł pierwszy')
+    except:
+        print('dłuższy driver nie zadziałał, lecimy dalej')
+        driver = u"{Microsoft Access Driver (*.mdb)}"
+
+    if os.path.exists(db_path):
+        connected = True
+        access_con_string = f"Driver={driver}; Dbq={db_path};"
+        connection = pyodbc.connect(access_con_string)
+        return connection, connected
+    else:
+        connected = False
+        return 'Ścieżka nie istnieje', connected
+
+
+def get_table_data(conn, sql, filter=None):
+    data = pd.read_sql(sql, conn, params=filter)
+    return data
+
+
+def shorten_adr_for(adr_for):
+    return adr_for[8:20].replace(' ', '')
+
+
+def to_excel(data, excel, sheet):
+    if sheet == 'LANDING_PAGE':
+        pass
+    else:
+        with pd.ExcelWriter(excel, mode='a', if_sheet_exists='overlay', engine='openpyxl') as writer:
+            result = data.to_excel(writer, header=False, index=False, sheet_name=sheet, startrow=3)
+        return result
+
+
+def set_style(wb, name_sh):
+    thin = Side(border_style="thin", color="000000")
+    workbook = openpyxl.load_workbook(wb)
+    for name in workbook.sheetnames:
+        sheet = workbook[name_sh]
+        for row in sheet.iter_rows(min_row=4, max_row=sheet.max_row, max_col=sheet.max_column):
+            for cell in row:
+                alignment = copy.copy(cell.alignment)
+                alignment.wrapText = True
+                cell.alignment = alignment
+                cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+    workbook.save(wb)
+
+
+def set_style(wb, name_sh):
+    thin = Side(border_style="thin",
+                color="000000")
+    allign = Alignment(horizontal = 'left',
+                       vertical='center',
+                       wrap_text=True,
+                       shrinkToFit=False)
+    workbook = openpyxl.load_workbook(wb)
+
+    sheet = workbook[name_sh] #dynamicznie przekaze w funkcji final
+
+    for row in sheet.iter_rows(min_row=4, max_row=sheet.max_row, max_col=sheet.max_column):
+        for cell in row:
+            #alignment = copy.copy(cell.alignment)
+            #alignment.wrapText = True
+            #cell.alignment = alignment
+            cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+            cell.alignment = allign
+
+    workbook.save(wb)
+
+# def set_format(wb, name_sh):
+#     workbook = openpyxl.load_workbook(wb)
+#     sheet = workbook[name_sh]
+#     headers = get_column_names(name_sh,workbook)
+#     print(headers)
+#     for row in sheet.iter_cols(min_row=4, max_row=sheet.max_row,max_col=sheet.max_column, min_col=2):
+#         if name_sh in ['L_ENERG_LS','ROZB']: #na sztywno - trzeba poprawic bo inaczje bedzie chujowo, przy ewentualnym recznym dodawaniu
+#             for cell in row:
+#                 cell.number_format ='0.0000'
+#         else:
+#             for cell in row:
+#                 cell.number_format ='0.00'
+#workbook.save(wb)
+
+def set_format(wb, name_sh, headers= None):
+    workbook = openpyxl.load_workbook(wb)
+    sheet = workbook[name_sh]
+    if headers:
+        for row in sheet.iter_rows(min_row=4, max_row=sheet.max_row, max_col=sheet.max_column):
+            if 'Jakość hodowlana' in headers:
+                row[headers['Jakość hodowlana']].number_format = '00'
+            if 'Zadrzewienie' in headers:
+                row[headers['Zadrzewienie']].number_format = '0.00'
+            if 'Pow.[ha]' in headers:
+                row[headers['Pow.[ha]']].number_format = '0.00'
+            if 'Pow.[ha] ' in headers:
+                row[headers['Pow.[ha] ']].number_format = '0.0000'
+    else:
+        for row in sheet.iter_rows(min_row=4, max_row=sheet.max_row,max_col=sheet.max_column, min_col=2):
+                for cell in row:
+                    cell.number_format = '0.00'
+
+    workbook.save(wb)
+
+def delete_empty_raports(excel, sheet_name_list):
+    wb = openpyxl.load_workbook(excel)
+    preserve = ['LANDING_PAGE']
+    for name in wb.sheetnames:
+        sheet = wb[name]
+        if name not in sheet_name_list and name not in preserve:
+            wb.remove_sheet(sheet)
+    wb.save(excel)
+
+def get_column_names(sheet_name,wb= None, excel = None): #zwraca nazwy kolumn (w excelu!!!) raz z ich indexem(numer kolumny ('nazwa'=index)
+    if excel:
+        wb = openpyxl.load_workbook(excel)
+    else:
+        wb = wb
+    sheet = wb[sheet_name]
+    headers = {}
+
+    for idx, col in enumerate(sheet.iter_cols(1,sheet.max_column, min_row=2),start=0):
+        headers[col[0].value] = idx
+    return headers
+
+
+def add_logo(excel, img_path, pos):
+    logo = img_path
+    wb = openpyxl.load_workbook(excel)
+    ws = wb['LANDING_PAGE']
+    img = openpyxl.drawing.image.Image(logo)
+    img.anchor = pos
+    ws.add_image(img)
+    wb.save(excel)
+
+
+def copy_blank_raport(root_dir, dst_dir, raport_name):  # tworzenie kopii "zapisywanie do wybranej sciezki"
+    raport_name = raport_name + '.xlsx'
+    scheme = os.path.join(root_dir, 'misc', 'SCHEME_PUL.xlsx')
+    new_name = os.path.join(dst_dir, raport_name)
+    blank_raport = shutil.copy2(scheme, dst_dir)
+    os.rename(blank_raport, new_name)
+    new_raport_dir = os.path.join(dst_dir, raport_name)
+    return new_raport_dir
+
+
+def delete_empty_raports(excel, sheet_name_list):
+    wb = openpyxl.load_workbook(excel)
+    preserve = ['LANDING_PAGE']
+    for name in wb.sheetnames:
+        sheet = wb[name]
+        if name not in sheet_name_list and name not in preserve:
+            wb.remove_sheet(sheet)
+    wb.save(excel)
+
+
+def add_logo(excel, img_path, pos):
+    logo = img_path
+    wb = openpyxl.load_workbook(excel)
+    ws = wb['LANDING_PAGE']
+    img = openpyxl.drawing.image.Image(logo)
+    img.anchor = pos
+    ws.add_image(img)
+    wb.save(excel)
+
+
+def set_bold(x):
+    return 'font-weight: bold'
+def set_font_size(x):
+    return "font-size: 8pt "
+
+def set_green(x):
+    return 'background-color:#005040'
+
+
+def set_white_text(x):
+    return 'color:#ffffff'
+
+
+def shorten_adr_for(adr_for):
+    return adr_for[8:20].replace(' ', '')
+
+
+def replace_by_dict(dict, excel_path, sheet):  # zmieniacz do strony tytulowej
+    wb = openpyxl.load_workbook(excel_path)
+    ws = wb[sheet]
+    for r in range(1, ws.max_row + 1):
+        for c in range(1, ws.max_column + 1):
+            s = ws.cell(r, c).value
+            for search_txt, replace_txt in dict.items():
+                if s != None and search_txt in s:
+                    ws.cell(r, c).value = s.replace(search_txt, replace_txt)
+    wb.save(excel_path)
+
+
+def add_header_footer(excel_path, sheetname):
+    wb = openpyxl.load_workbook(excel_path)
+    ws = wb[sheetname]
+    ws.oddHeader.left.text = "Page &[Page] of &N"
+    ws.oddHeader.left.size = 8
+    ws.oddHeader.left.font = "Calibri"
+    ws.oddHeader.left.color = "CC3366"
+    wb.save(excel_path)
+
+
+def set_bold(x):
+    return 'font-weight: bold'
+
+
+def set_green(x):
+    return 'background-color:#005040'
+
+
+def set_white_text(x):
+    return 'color:#ffffff'
+
+
+def set_borders(dataframe):
+    sf = StyleFrame(dataframe)
+    sf.apply_column_style(cols_to_style=dataframe.columns,
+                          styler_obj=Styler(bg_color=utils.white, font=utils.fonts.calibri, font_size=8),
+                          style_header=False)
